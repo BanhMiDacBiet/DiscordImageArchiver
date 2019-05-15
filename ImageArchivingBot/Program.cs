2219ee1b8e409b55180492b320dfe3edb7b3cb2c
jl91569 <10607947+jl91569@users.noreply.github.com>
2019-05-15 22:45:34 +1000

    Add project files.

---
+++ ImageArchivingBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageArchivingBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
